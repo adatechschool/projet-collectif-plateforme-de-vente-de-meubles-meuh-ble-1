28a8adf9cddbd72c803c3f39bcc157084549e17d
Toto <tototuri2022@gmail.com>
2023-10-30 15:03:27 +0100

    toto

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const app = express();
-
-const port =  3004;
-
+const port =  3005;
 app.use(express.json());
-
-
 app.get('/', (req,res) =>{
     console.log(req.query);
     res.send('Voci la réponse du serveur Express !');
 })
-
 app.get('*', (req,res) =>{
     res.status(404).send('error');
 })
-
 app.post('/',(req,res) =>{
     console.log(req.body);
     res.send('ok')
-
 })
-
 app.listen(port, () => {
     console.log(`Le serveur Express est en cours d'écoute sur le port  ${port}`);
   });
+
+
+
+
+
+
+
 
 
