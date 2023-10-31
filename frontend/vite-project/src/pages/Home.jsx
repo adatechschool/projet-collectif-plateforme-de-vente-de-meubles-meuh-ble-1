36507f1d7969732ba8764b74479419d3ff81cb98
MikeMaille88 <mickael.bohrer@gmail.com>
2023-10-31 16:52:21 +0100

    homepage banner and carousel init

--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -1,5 +1,51 @@
+import Image from "react-bootstrap/Image";
+import banniereImage from "../images/BANNIERE_meubles-1210x423.png";
+import Carousel from "react-bootstrap/Carousel";
+
 const Home = () => {
-  return <h2>Accueil</h2>;
+  return (
+    <>
+      <Image src={banniereImage} fluid />
+
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banniereImage}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banniereImage}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banniereImage}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Home;
