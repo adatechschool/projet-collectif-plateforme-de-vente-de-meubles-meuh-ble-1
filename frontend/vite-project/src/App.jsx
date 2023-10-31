e2eeb759722d1ddc4bad5a3f486d1752a176634f
MikeMaille88 <mickael.bohrer@gmail.com>
2023-10-31 14:25:20 +0100

    navbar init

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,15 +1,30 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Product from "./pages/Product.jsx";
 import Admin from "./pages/Admin.jsx";
+import Navbar from "./components/navbar.jsx";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    );
+  };
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/product", element: <Product /> },
+        { path: "/admin", element: <Admin /> },
+      ],
+    },
     { path: "/login", element: <Login /> },
-    { path: "/product", element: <Product /> },
-    { path: "/admin", element: <Admin /> },
   ]);
   return <RouterProvider router={router} />;
 }
---
+++ frontend/vite-project/src/components/navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <h1>Meuh-Blé</h1>;
+};
+
+export default Navbar;
