b3e415cc67f0103a9e199d8e70ed2cf9fbd2ae22
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-02 10:43:15 +0100

    homepage structure and components

--- frontend/vite-project/src/components/navbar.jsx
+++ frontend/vite-project/src/components/navbar.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
 const NavigationBar = () => {
   return (
@@ -10,6 +11,9 @@ const NavigationBar = () => {
         <Container>
           <Navbar.Brand href="#home">Meuh-Blé</Navbar.Brand>
         </Container>
+        <Button variant="primary" style={{ marginRight: "2rem" }}>
+          Login/Logout
+        </Button>{" "}
       </Navbar>
     </>
   );
