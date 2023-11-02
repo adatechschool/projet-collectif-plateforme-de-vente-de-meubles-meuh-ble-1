223c7b9be19754f4dd55b58e3d0882d4a1c853e9
Walid Jeghnine <jeghninewalid@outlook.fr>
2023-11-02 14:26:56 +0100

    conflict

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ mongoose.connect(process.env.MONGODB_URL, {
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((e) => console.log('Connexion à MongoDB échouée !' + e));
 
-<<<<<<< HEAD
-=======
 
 mongoose.connect(process.env.MONGODB_URL,
 { useNewUrlParser: true,
@@ -24,13 +22,11 @@ mongoose.connect(process.env.MONGODB_URL,
 
 
 
->>>>>>> aa389e32fdb090845f8facb06f7dd7c2d75c20b0
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/products', productRoute);
 app.use('/registrer', registerRoute); // Ajout de la route pour l'inscription
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 3005; // Port sur lequel le serveur écoutera
 
 app.listen(PORT, () => {
@@ -47,7 +43,6 @@ app.listen(PORT, () => {
 
 
 
-=======
 app.use('/user', userRoute);
 app.use("/product", productRoute);
 
@@ -66,4 +61,3 @@ app.post('/',(req,res) =>{
 app.listen(process.env.PORT, () => {
     console.log(`Le serveur Express est en cours d'écoute sur le port ${process.env.PORT} `);
   });
->>>>>>> aa389e32fdb090845f8facb06f7dd7c2d75c20b0
