0763af20ad62bac2f656a3352a7d479b5a38fe64
Toto <tototuri2022@gmail.com>
2023-11-07 16:18:24 +0100

    getting user login data

--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -5,6 +5,51 @@ import Container from "react-bootstrap/Container";
 //TODO : lien vers page SignIn
 
 function Login() {
+
+    async function log(event){
+      
+      event.preventDefault();
+
+        const email = event.target[0].value;
+        const password = event.target[1].value;
+
+          if (email.trim().length === 0 || password.trim().length === 0) {
+              alert("All fields are required.");
+          } else {
+              console.log("Email:", email);
+              console.log("Password:", password);
+              
+      try {
+        console.log("dream")
+        const response = await fetch(`http://localhost:${import.meta.env.VITE_APP_PORT}/auth`, {
+              method: "POST",
+              body: JSON.stringify({
+              email: email,
+              password: password
+              }),
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                  },
+                  });
+                  
+              if (response.ok){
+                  console.log(response);
+                  const result = await response.json();
+                  
+                  console.log(result.authToken)
+                  localStorage.setItem("token", JSON.stringify(result.authToken))
+                // const token = JSON.parse(localStorage.getItem("token"))
+                // console.log(token)
+                }
+                
+      } catch (error) {
+        console.log("toto",error.message) }
+    
+      }
+
+    }
+  
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
@@ -16,19 +61,10 @@ function Login() {
       <Form
         className="w-50"
         onSubmit={(event) => {
-          event.preventDefault();
-
-          const email = event.target[0].value;
-          const password = event.target[1].value;
-
-          if (email.trim().length === 0 || password.trim().length === 0) {
-            alert("All fields are required.");
-          } else {
-            console.log("Email:", email);
-            console.log("Password:", password);
-          }
+          log(event);
         }}
       >
+
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
