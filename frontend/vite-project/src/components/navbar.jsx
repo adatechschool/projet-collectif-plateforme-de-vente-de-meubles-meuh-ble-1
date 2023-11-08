d75f81310b00ca0d6d5da60a954e5168939d028c
MikeMaille88 <mickael.bohrer@gmail.com>
2023-11-08 17:01:24 +0100

    navbar update to user

--- frontend/vite-project/src/components/navbar.jsx
+++ frontend/vite-project/src/components/navbar.jsx
@@ -1,26 +1,61 @@
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
+import React from "react";
+import { MDBBtn, MDBIcon, MDBBadge } from "mdb-react-ui-kit";
 
 //TODO :
 // Si user non connecté => login sinon => logout et PP (lien vers panier ?)
 // Si user = admin => PP ou bouton lien vers interface admin
 
 const NavigationBar = () => {
-  return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/">Meuh-Blé</Navbar.Brand>
-        </Container>
-        <Button variant="primary" style={{ marginRight: "2rem" }} href="/login">
-          Login/Logout
-        </Button>{" "}
-      </Navbar>
-    </>
-  );
+  const token = JSON.parse(localStorage.getItem("token"));
+  const cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!token) {
+    return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Meuh-Blé</Navbar.Brand>
+          </Container>
+          <Button
+            variant="primary"
+            style={{ marginRight: "2rem" }}
+            href="/login"
+          >
+            Login
+          </Button>{" "}
+        </Navbar>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Meuh-Blé</Navbar.Brand>
+          </Container>
+          <MDBBtn
+            style={{ backgroundColor: "#817c79", marginRight: "0.5rem" }}
+            href="/cart"
+          >
+            <MDBIcon fas icon="shopping-cart" />
+            <MDBBadge color="danger" className="ms-2">
+              {cart.length}
+            </MDBBadge>
+          </MDBBtn>
+          <Button
+            variant="primary"
+            style={{ marginRight: "2rem" }}
+            href="/login"
+          >
+            Logout
+          </Button>{" "}
+        </Navbar>
+      </>
+    );
+  }
 };
 
 export default NavigationBar;
